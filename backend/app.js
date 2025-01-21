056a6310da6e0e846ee69de90fa904d0d3c03108
fatima <fatimasaleem778899@gmail.com>
2025-01-21 20:43:22 +0500

    changed

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+require('dotenv').config();
 const userRouter = require("./router/employeeRoute");
 const connectdb = require("./db")
 const cors = require("cors")
 
 app.use(express.json());
 app.use('/api', userRouter);
-app.use(cors())
+app.use(cors({ origin: env.process.FRONTEND_URL }))
 connectdb()
 
 
